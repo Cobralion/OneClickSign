a21c11b80ec07d7067171d9edeec621fe11ece4f
Maurice Lindner <lindner.maurice@freenet.de>
2020-08-21 00:40:48 +0200

    Added PDF Viewer

---
+++ OneClickSign/OneClickSign/Data/Submit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneClickSign.Data
+{
+    public class Submit
+    {
+        [Required]
+        [Range(typeof(bool), "true", "true",
+        ErrorMessage = "You have to agree to proceed.")]
+        public bool HasAgreed { get; set; }
+    }
+}
--- OneClickSign/OneClickSign/wwwroot/js/index.js
+++ OneClickSign/OneClickSign/wwwroot/js/index.js
@@ -1,4 +1,31 @@
 ﻿
-function initReader(address) {
+function initReader(address, element, options) {
+    if (!options) {
+        options = {
+            pdfOpenParams: { scrollbar: '1', toolbar: '0', statusbar: '0', messages: '0', navpanes: '0' }
+        };
+    }
 
+    let elementID = element.id;
+
+    console.log(`id is: ${elementID}`);
+
+    if ($(elementID).length == 0) {
+        elementID = uniqid();
+        $(element).attr('id', elementID);
+    }
+
+    PDFObject.embed(address, `#${elementID}`, options);
+}
+
+function uniqid(a = "", b = false) {
+    const c = Date.now() / 1000;
+    let d = c.toString(16).split(".").join("");
+    while (d.length < 14) d += "0";
+    let e = "";
+    if (b) {
+        e = ".";
+        e += Math.round(Math.random() * 100000000);
+    }
+    return `id${(a + d + e).toString()}`;
 }
