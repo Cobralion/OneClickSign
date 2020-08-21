4141458e2e1cd50972f4a59cdaf5a28820215bec
Maurice Lindner <lindner.maurice@freenet.de>
2020-08-21 21:33:24 +0200

    Done

---
+++ OneClickSign/OneClickSign/Data/Static.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneClickSign.Data
+{
+    public static class Static
+    {
+        public static Uri PDFUri
+        {
+            get => new Uri("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf");
+        }
+    }
+}
